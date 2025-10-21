e18b9d52119ba2d84f0e8e66ba37d1dd64ed18ef
Peter <peter.mahoney923@gmail.com>
2025-10-20 22:56:48 -0700

    Fix Flask app for Render

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import os
 import requests
 from google.transit import gtfs_realtime_pb2
 from datetime import datetime, timezone
@@ -37,4 +38,6 @@ def nextbus():
     return jsonify({"next": departures})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
